f51bd08eec2ccdf12b5a5405b7f69133a9b214e2
zhangzhiheng <zhangzhiheng>
2019-10-19 17:01:07 +0800

    初始化

--- life-weather/src/main/java/apple/pangzi/weatcher/WeatherApplication.java
+++ life-weather/src/main/java/apple/pangzi/weatcher/WeatherApplication.java
@@ -3,9 +3,11 @@ package apple.pangzi.weatcher;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class WeatherApplication {
 
     public static void main(String[] args) {
