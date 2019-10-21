d6cd4ab7d30203277ac835dc7a0b1833067a81ec
zhangzhiheng <zhangzhiheng>
2019-10-21 18:07:05 +0800

    初始化

--- life-weather/src/main/java/apple/pangzi/weatcher/service/TestServiceImpl.java
+++ life-weather/src/main/java/apple/pangzi/weatcher/service/TestServiceImpl.java
@@ -1,7 +1,9 @@
 package apple.pangzi.weatcher.service;
 
 import apple.pangzi.weather.api.service.TestService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TestServiceImpl implements TestService {
     @Override
     public String test() {
