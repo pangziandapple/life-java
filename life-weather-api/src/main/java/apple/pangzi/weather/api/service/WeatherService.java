83cda1db1568a8666e1483026f6f7c7d7939e79f
zhangzhiheng <zhangzhiheng>
2019-10-23 15:53:41 +0800

    初始化

--- life-weather-api/src/main/java/apple/pangzi/weather/api/service/WeatherService.java
+++ life-weather-api/src/main/java/apple/pangzi/weather/api/service/WeatherService.java
@@ -3,7 +3,7 @@ package apple.pangzi.weather.api.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("/v1/life-weather")
+@FeignClient("life-weather")
 public interface WeatherService {
 
     @GetMapping("/weather")
--- life-weather-api/src/main/java/apple/pangzi/weather/api/service/WeatherV2Service.java
+++ life-weather-api/src/main/java/apple/pangzi/weather/api/service/WeatherV2Service.java
@@ -3,9 +3,9 @@ package apple.pangzi.weather.api.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("/v2/life-weather")
+@FeignClient("life-weather")
 public interface WeatherV2Service {
 
-    @GetMapping("/weather")
+    @GetMapping("/weather/v2")
     String getWeather();
 }
