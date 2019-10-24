187c7f7d975c2bad72c021d6dcdbe5e71322f6bf
zhangzhiheng <zhangzhiheng>
2019-10-24 16:19:54 +0800

    增加多环境

---
+++ life-weather/src/main/java/apple/pangzi/weatcher/controller/WeatherController.java
@@ -0,0 +1,13 @@
+package apple.pangzi.weatcher.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WeatherController {
+
+    @GetMapping("/weather")
+    public String weather(){
+        return "weather";
+    }
+}
