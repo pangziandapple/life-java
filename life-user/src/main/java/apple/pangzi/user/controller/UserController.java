cd3eba8b46ba57935b21cee771d8444f2af0c169
appleandpangzi <154369055@qq.com>
2020-01-28 15:40:16 +0800

    初始化mybatis

--- life-user/src/main/java/apple/pangzi/user/controller/UserController.java
+++ life-user/src/main/java/apple/pangzi/user/controller/UserController.java
@@ -1,14 +1,21 @@
 package apple.pangzi.user.controller;
 
 import apple.pangzi.common.view.model.ApiResult;
+import apple.pangzi.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    @PostMapping("/login")
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/login")
     public ApiResult login() {
+        userMapper.getByMobile("");
         return ApiResult.success();
     }
 }
